a97ef7e7658aac9a9ff5da1003c787fc0be0ecad
wiliamsova01-debug <wiliamsova01@gmail.com>
2025-12-12 20:49:36 +0700

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,3 +1,13 @@
-import db from '@/lib/db';
+import { PrismaClient } from '@prisma/client'
 
-export default db;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const db =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
